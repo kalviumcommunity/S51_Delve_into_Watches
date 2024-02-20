9d0bb638b023c455ebedcb1e8c61ac0939c470ec
adhithyaa <adhthyaa.mv@kalvium.community>
2024-02-20 14:18:45 +0530

    watches

---
+++ db.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const mongoURI= "mongodb+srv://Adhithyaa:adhi@adhithyaa.k4o655v.mongodb.net/?retryWrites=true&w=majority"
+
+
+const startDatabase = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log('📦 connected to MongoDB');
+  } catch (err) {
+    console.error('❌ error connecting to MongoDB:', err.message);
+  }
+};
+
+const stopDatabase = async () => {
+  try {
+    await mongoose.disconnect();
+    console.log('📦 disconnected from MongoDB');
+  } catch (err) {
+    console.error('❌ error disconnecting from MongoDB:', err.message);
+  }
+};
+
+const isConnected = () => {
+  return mongoose.connection.readyState === 1;
+}
+
+module.exports = { startDatabase, stopDatabase, isConnected };
