18ddb82b3c4ba5e0fe447af3a172f81b59a71797
adhithyaa <adhthyaa.mv@kalvium.community>
2024-02-23 09:53:51 +0530

    Created my react app

---
+++ client/Delve-into-Watches/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Welcome to Delve Into Watches</h1>
+      <p>In this website you can time travel with the history of watches till present and can know about the history of watches.</p>
+      
+    </>
+  )
+}
+
+export default App
